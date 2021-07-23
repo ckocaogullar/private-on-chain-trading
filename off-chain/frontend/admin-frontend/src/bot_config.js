432ab2ecef0492ccd384223047be3f418549aaa0
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2021-07-23 14:29:34 +0100

    Implemented the manual trading logic

--- off-chain/frontend/admin-frontend/src/bot_config.js
+++ off-chain/frontend/admin-frontend/src/bot_config.js
@@ -1,10 +1,31 @@
 // Change the CONTRACT_ADDRESS and ABI appropriately, as described in the README
 
-export const BOT_CONTRACT_ADDRESS = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
+export const BOT_CONTRACT_ADDRESS = '0x4826533B4897376654Bb4d4AD88B7faFD0C98528'
 
 export const BOT_ABI = [
   {
-    "inputs": [],
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_uniswapV3Factory",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_token0",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "_token1",
+        "type": "address"
+      },
+      {
+        "internalType": "uint24",
+        "name": "_defaultFee",
+        "type": "uint24"
+      }
+    ],
     "stateMutability": "nonpayable",
     "type": "constructor"
   },
@@ -14,7 +35,7 @@ export const BOT_ABI = [
       {
         "indexed": false,
         "internalType": "address",
-        "name": "subscriberAddress",
+        "name": "userAddress",
         "type": "address"
       }
     ],
@@ -35,9 +56,135 @@ export const BOT_ABI = [
     "type": "event"
   },
   {
+    "inputs": [
+      {
+        "internalType": "uint32",
+        "name": "numOfPeriods",
+        "type": "uint32"
+      },
+      {
+        "internalType": "uint32",
+        "name": "periodLength",
+        "type": "uint32"
+      }
+    ],
+    "name": "bollinger",
+    "outputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "pastPrices",
+        "type": "uint256[]"
+      },
+      {
+        "internalType": "uint256",
+        "name": "movingAverage",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "stdDev",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "defaultFee",
+    "outputs": [
+      {
+        "internalType": "uint24",
+        "name": "",
+        "type": "uint24"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getCurrentPrice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "currentPrice",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "quoter",
+    "outputs": [
+      {
+        "internalType": "contract IQuoter",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
     "inputs": [],
     "name": "requestSubscription",
     "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint32",
+        "name": "numOfPeriods",
+        "type": "uint32"
+      },
+      {
+        "internalType": "uint32",
+        "name": "periodLength",
+        "type": "uint32"
+      }
+    ],
+    "name": "sma",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "sma",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "priceAtTick",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "pastPrices",
+        "type": "uint256[]"
+      },
+      {
+        "internalType": "uint256",
+        "name": "mean",
+        "type": "uint256"
+      }
+    ],
+    "name": "standardDev",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "stdDev",
+        "type": "uint256"
+      }
+    ],
     "stateMutability": "nonpayable",
     "type": "function"
   },
@@ -52,6 +199,112 @@ export const BOT_ABI = [
     "name": "subscribeUser",
     "outputs": [],
     "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "tokenIn",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "tokenOut",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "swap",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "test",
+    "outputs": [
+      {
+        "internalType": "uint32",
+        "name": "val",
+        "type": "uint32"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "token0",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "token1",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint32",
+        "name": "numOfPeriods",
+        "type": "uint32"
+      },
+      {
+        "internalType": "uint32",
+        "name": "periodLength",
+        "type": "uint32"
+      }
+    ],
+    "name": "trade",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "uniswapRouter",
+    "outputs": [
+      {
+        "internalType": "contract ISwapRouter",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "uniswapV3Factory",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
     "type": "function"
   }
 ]
--- on-chain/contracts/BaseBot.sol
+++ on-chain/contracts/BaseBot.sol
@@ -15,18 +15,25 @@ import "./libraries/SafeMath32.sol";
 
 contract BaseBot {
     // Administrator of the trading bot
+    using SafeMath for uint256;
+
     address admin;
 
-    // Maps a user's address to the amount she promises to invest in the algorithm, keeping the investment amount private to the admin
-    mapping(address => uint32) investmentAmount;
-    // Maps a user's address to the amount she receives as the result of trading, keeping the share private to the corresponding user
-    mapping(address => uint32) userShare;
+    ISwapRouter public constant uniswapRouter =
+        ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+    IQuoter public constant quoter =
+        IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
 
-    // zkay v1 does not support arrays. As I could not confirm yet that zkay v2 supports them,
-    // I could not stop myself from being safe and representing lists with mapping(uint => address)s and counters.
+    address public immutable uniswapV3Factory;
+    address public immutable token0;
+    address public immutable token1;
+    uint24 public immutable defaultFee;
+
     address[] pendingSubscribers;
 
     address[] confirmedSubscribers;
+
+    address poolAddress;
 
     enum SubscriptionStatus {
         PENDING,
@@ -43,15 +50,27 @@ contract BaseBot {
 
     mapping(address => Subscriber) subscribers;
 
-    constructor() {
+    constructor(
+        address _uniswapV3Factory,
+        address _token0,
+        address _token1,
+        uint24 _defaultFee
+    ) {
+        uniswapV3Factory = _uniswapV3Factory;
+        token0 = _token0;
+        token1 = _token1;
+        defaultFee = _defaultFee;
         admin = msg.sender;
+
     }
+
+    // ------------------------------- SUBSCRIPTION -------------------------------
 
     // @notice Request subscription to the bot with an amount of investment. This function does not transfer any investment to the admin
     // @param _investmentAmount This is the amount of money the user promises to send the admin
     function requestSubscription() external {
         // Make sure that the one calling this function is a user, not the admin
-        require(msg.sender != admin);
+        require(msg.sender != admin, "Admin cannot subscribe to the bot");
         // Add the caller to the subscribers list, with status pending confirmation
         subscribers[msg.sender].subscriptionStatus = SubscriptionStatus.PENDING;
         emit UserSubscribed(msg.sender);
@@ -62,9 +81,198 @@ contract BaseBot {
     // Otherwise, the admin can pay back the amount using an external method
     function subscribeUser(address user) external {
         // Make sure that the one calling this function is the admin
-        require(msg.sender == admin);
+        require(msg.sender == admin, "Only the admin can confirm subscription");
         // Check if the user has actually invested the money she hes promised to invest
         subscribers[user].subscriptionStatus = SubscriptionStatus.APPROVED;
         emit SubscriptionConfirmed(user);
+    }
+
+    // ------------------------------- TRADING -------------------------------
+
+    // --------------------------- THE MAIN TRADING FUNCTION ---------------------------
+    /// Trading
+
+    // Called by the admin to make trading decisions.
+    // Calls the functions for separate trading algorithms
+    function trade(uint32 numOfPeriods, uint32 periodLength) public {
+        // Make sure that the one who is calling the algorithm is the admin of the bot
+        require(msg.sender == admin, "Only the admin can invoke trading");
+        // The bot admin selects the trading algorithms secretly in the following way:
+        // It calls the algorithms in all cases
+        // If the first parameter passed to these functions are negative, this is an indicator that this algorithm is going to be used or not
+        // This is checked in the algorithm function and the algorithm proceeds or does not proceed accordingly
+        bollinger(numOfPeriods, periodLength);
+
+        // Update the value of the amount of the money in the pool after trading. Of course, this should happen after
+        // the trades are executed externally
+    }
+
+    // --------------------------- TRADING ALGORITHM: BOLLINGER ---------------------------
+    // @notif Buy if (price < lower Bolliger band), sell if (price > upper Bolliger band) within certain thresholds
+    //
+    // Public parameters:
+    // @param numOfPeriods number of periods, same as explained above
+    // @param periodLength length of the periods for both EMAs, same as explained above
+    //
+    // Private parameters:
+    // @private_param bollingerUpperBoundPct percentage the current price should be near the upper Bollinger band to trigger a sell signal
+    // @private_param bollingerLowerBoundPct percentage the current price should be near the lower Bollinger band to trigger a buy signal
+    function bollinger(uint32 numOfPeriods, uint32 periodLength)
+        public
+        returns (
+            uint256[] memory pastPrices,
+            uint256 movingAverage,
+            uint256 stdDev
+        )
+    {
+        require(
+            admin == msg.sender,
+            "Only the admin can invoke the trading algorithm"
+        );
+        // If the first parameter is negative, this is an indicator that this algorithm is going to be used or not
+        require(numOfPeriods > 0);
+        // Upper Bollinger Band=MA(TP,n) + m * σ[TP,n]
+        // Lower Bollinger Band=MA(TP,n) − m * σ[TP,n]
+        //      where:
+        //      MA=Moving average
+        //      TP (typical price)=(High+Low+Close)÷3
+        //      n=Number of days in smoothing period (typically 20)
+        //      m=Number of standard deviations (typically 2)
+        //      σ[TP,n]=Standard Deviation over last n periods of TP
+        (movingAverage, pastPrices) = sma(numOfPeriods, periodLength);
+        stdDev = standardDev(pastPrices, movingAverage);
+        // upperBollingerBand = movingAverage + 2 * stdDev;
+        // lowerBollingerBand = movingAverage - 2 * stdDev;
+        // if (currentPrice > (upperBollingerBand / 100) * (100 - bollingerUpperBoundPct)) {
+        //     console.log("Selling token1");
+        //     //swap(token1, token0, subscribers[msg.sender].amount);
+        // } else if (currentPrice < (lowerBollingerBand / 100) * (100 + bollingerLowerBoundPct)) {
+        //     console.log("Buying token1");
+        //     //swap(token0, token1, subscribers[msg.sender].amount);
+        // }
+        // else: Hold
+    }
+
+    function test() public returns(uint32 val){
+        val = 1111;
+    }
+
+    function getCurrentPrice() public returns (uint256 currentPrice) {
+        if (poolAddress == address(0x0)) {
+            poolAddress = PoolAddress.computeAddress(
+                uniswapV3Factory,
+                PoolAddress.getPoolKey(token0, token1, defaultFee)
+            );
+        }
+        int256 twapTick = OracleLibrary.consult(poolAddress, 1);
+        currentPrice = OracleLibrary.getQuoteAtTick(
+            int24(twapTick), // can assume safe being result from consult()
+            1,
+            token0,
+            token1
+        );
+
+        console.log("Current price is %", currentPrice);
+    }
+
+    function swap(
+        address tokenIn,
+        address tokenOut,
+        uint256 amount
+    ) public {
+        require(
+            ((tokenIn == token0 && tokenOut == token1) ||
+                (tokenIn == token1 && tokenOut == token0)),
+            "Input tokens are wrong."
+        );
+        // if (tokenIn == token0 && tokenOut == token1) {
+        //     subscribers[msg.sender].balanceToken0 -= amount;
+        //     subscribers[msg.sender].balanceToken1 += amount;
+        // } else {
+        //     subscribers[msg.sender].balanceToken1 -= amount;
+        //     subscribers[msg.sender].balanceToken0 += amount;
+        // }
+        // console.log("Balance of token0 is % , token1 is %", subscribers[msg.sender].balanceToken0, subscribers[msg.sender].balanceToken1);
+    }
+
+    // Helper functions
+
+    /// @notice Given a time period to look back into and the number of data points, calculates the
+    /// Simple Moving Average (SMA) for token1 in terms of token0
+    /// @return sma SMA, i.e. average of a number of past prices
+    function sma(uint32 numOfPeriods, uint32 periodLength)
+        public
+        returns (uint256 sma, uint256[] memory priceAtTick)
+    {
+        // Calculate the time intervals to look behind untilSecondsAgo with numIntervals
+        // Put this information into secondAgos for feeding into pool observation later
+        uint32[] memory secondAgos = new uint32[](numOfPeriods + 1);
+        for (uint32 i = 0; i < numOfPeriods; i++) {
+            secondAgos[i] = SafeMath32.mul(periodLength, numOfPeriods - i);
+        }
+
+        // Get the Uniswap Pool for token0, token1, and defaultFee values
+        IUniswapV3Pool pool = IUniswapV3Pool(poolAddress);
+        (int56[] memory tickCumulatives, ) = pool.observe(secondAgos);
+
+        // Calculate the Simple Moving Average by adding prices (TWAPs) for the time intervals and dividing
+        // that by the number of intervals
+        sma = 0;
+
+        // Get prices at each tick
+        priceAtTick = new uint256[](tickCumulatives.length - 1);
+        for (uint32 i = 0; i < tickCumulatives.length - 1; i++) {
+            int56 tickCumulativesDelta = tickCumulatives[
+                tickCumulatives.length - 1 - i
+            ] - tickCumulatives[tickCumulatives.length - 2 - i];
+            int24 timeWeightedAverageTick = int24(
+                tickCumulativesDelta / periodLength
+            );
+            // Always round to negative infinity (taken from Uniswap OracleLibrary's consult())
+            if (
+                tickCumulativesDelta < 0 &&
+                (tickCumulativesDelta % periodLength != 0)
+            ) timeWeightedAverageTick--;
+
+            // Calculate the amount of token1 received in exchange for token0
+            priceAtTick[i] = OracleLibrary.getQuoteAtTick(
+                timeWeightedAverageTick,
+                1,
+                token0,
+                token1
+            );
+
+            // Add the learned price to SMA
+            sma = SafeMath.add(sma, priceAtTick[i]);
+        }
+        // Divide SMA with the number of intervals
+        sma = sma.div(tickCumulatives.length - 1);
+    }
+
+    // Standard deviation of the prices in numOfPeriods periods of periodLength length. Also takes mean as this value is precomputed in the
+    // bollinger function and used in several other places
+    function standardDev(uint256[] memory pastPrices, uint256 mean)
+        public
+        returns (uint256 stdDev)
+    {
+        uint256 sumOfSquaredDev = 0;
+        for (uint256 i = 0; i < pastPrices.length; i++) {
+            sumOfSquaredDev += (pastPrices[i] - mean) * (pastPrices[i] - mean);
+        }
+        sqrt(sumOfSquaredDev / pastPrices.length);
+    }
+
+    // Maths
+    function sqrt(uint256 y) internal pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
     }
 }
