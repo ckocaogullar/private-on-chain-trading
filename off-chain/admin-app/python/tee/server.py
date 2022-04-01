2f4949c11a08bced4b3c36920782b5461348ad07
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2022-04-01 19:21:21 +0100

    Attempting TEE - host connection 12

--- off-chain/admin-app/python/tee/server.py
+++ off-chain/admin-app/python/tee/server.py
@@ -54,6 +54,7 @@ class VsockListener:
         self.sock.listen(self.conn_backlog)
 
     def recv_data(self):
+        global witness_input
         # Receive data from a remote endpoint
         while True:
             try:
@@ -68,7 +69,7 @@ class VsockListener:
                 # Call the external URL
                 # for our scenario we will download list of published ip ranges and return list of S3 ranges for porvided region.
                 trigger_trade(10, 10)
-
+                print('Sending witness input:', witness_input)
                 # Send back the response
                 from_client.send(str(witness_input).encode())
 
