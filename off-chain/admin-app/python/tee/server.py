f19674e561d10571230c233f3d4fb3ac7362bcf0
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2022-04-06 15:29:37 +0100

    Reduced unused content

--- off-chain/admin-app/python/tee/server.py
+++ off-chain/admin-app/python/tee/server.py
@@ -2,7 +2,7 @@
 # // SPDX-License-Identifier: MIT-0
 import argparse
 import socket
-import config
+# import config
 from threading import Thread
 import time
 import sys
@@ -20,6 +20,328 @@ try:
     from web3.gas_strategies.time_based import fast_gas_price_strategy
 except Exception as e:
     print(e)
+
+# Alchemy Ropsten URL
+URL = "https://eth-ropsten.alchemyapi.io/v2/fBCbSZh46WyftFgzBU-a8_tIgCCxEL22"
+
+# Hardhat node URL
+# URL = 'http://127.0.0.1:8545/'
+
+# Bollinger variables:
+UPPER_BOUND_PERCENTAGE = 90
+LOWER_BOUND_PERCENTAGE = 90
+
+BOT_CONTRACT_ADDRESS = '0x358B7D1428270063a860647DB201EcDf03E2AB64'
+
+# Account and its private key
+ACCOUNT = '0x569ECED9B05495f8D0766bd3A771F16BdC8b18C3'
+ACCOUNT_KEY = 'bc6d600f6bf2a5ad83377dd8743e5fe30b14064ea8e082f3a83ee704cca0cfc0'
+
+# Deversifi Signing Key
+STARK_PRIVATE_KEY = '743a0ff439f6ed52ed1fef395d6cea58af02088c91528b07048cc5f922d3f65'
+STARK_PUBLIC_KEY = '0x00894dc6ae7cb67ba4ee649d3d46afd07c60502fb3477923b1f8185fb02d3580'
+
+# Ropsten address for the previous contract version
+# BOT_CONTRACT_ADDRESS = '0x97dEF834E0fd1e6325235850ee2bA3192A5f0d77'
+
+# Hardhat Node address regularly used for this contract
+# BOT_CONTRACT_ADDRESS = '0xE05FB6Ef9451D4f459893f35241b77E689b97ccF'
+
+BOT_ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_uniswapV3Factory",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_token0",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "_token1",
+                "type": "address"
+            },
+            {
+                "internalType": "uint24",
+                "name": "_defaultFee",
+                "type": "uint24"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": False,
+        "inputs": [
+            {
+                "indexed": False,
+                "internalType": "uint256",
+                "name": "upperBollingerBand",
+                "type": "uint256"
+            },
+            {
+                "indexed": False,
+                "internalType": "uint256",
+                "name": "lowerBollingerBand",
+                "type": "uint256"
+            },
+            {
+                "indexed": False,
+                "internalType": "uint256",
+                "name": "currentPrice",
+                "type": "uint256"
+            }
+        ],
+        "name": "BollingerIndicators",
+        "type": "event"
+    },
+    {
+        "anonymous": False,
+        "inputs": [
+            {
+                "indexed": False,
+                "internalType": "bool",
+                "name": "verified",
+                "type": "bool"
+            }
+        ],
+        "name": "ProofVerified",
+        "type": "event"
+    },
+    {
+        "anonymous": False,
+        "inputs": [
+            {
+                "indexed": False,
+                "internalType": "uint24",
+                "name": "test",
+                "type": "uint24"
+            }
+        ],
+        "name": "TestEvent",
+        "type": "event"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint32",
+                "name": "numOfPeriods",
+                "type": "uint32"
+            },
+            {
+                "internalType": "uint32",
+                "name": "periodLength",
+                "type": "uint32"
+            }
+        ],
+        "name": "bollinger",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint32",
+                "name": "numOfPeriods",
+                "type": "uint32"
+            },
+            {
+                "internalType": "uint32",
+                "name": "periodLength",
+                "type": "uint32"
+            }
+        ],
+        "name": "calculateIndicators",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "defaultFee",
+        "outputs": [
+            {
+                "internalType": "uint24",
+                "name": "",
+                "type": "uint24"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getCurrentPrice",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "currentPrice",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "quoter",
+        "outputs": [
+            {
+                "internalType": "contract IQuoter",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint32",
+                "name": "numOfPeriods",
+                "type": "uint32"
+            },
+            {
+                "internalType": "uint32",
+                "name": "periodLength",
+                "type": "uint32"
+            }
+        ],
+        "name": "sma",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "sma",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256[]",
+                "name": "priceAtTick",
+                "type": "uint256[]"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256[]",
+                "name": "pastPrices",
+                "type": "uint256[]"
+            },
+            {
+                "internalType": "uint256",
+                "name": "mean",
+                "type": "uint256"
+            }
+        ],
+        "name": "standardDev",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "stdDev",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "test",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token0",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token1",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256[2]",
+                "name": "a",
+                "type": "uint256[2]"
+            },
+            {
+                "internalType": "uint256[2][2]",
+                "name": "b",
+                "type": "uint256[2][2]"
+            },
+            {
+                "internalType": "uint256[2]",
+                "name": "c",
+                "type": "uint256[2]"
+            },
+            {
+                "internalType": "uint256[4]",
+                "name": "inputs",
+                "type": "uint256[4]"
+            }
+        ],
+        "name": "trade",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "uniswapRouter",
+        "outputs": [
+            {
+                "internalType": "contract ISwapRouter",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "uniswapV3Factory",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+
 
 upperBoundPercentage = 100
 lowerBoundPercentage = 100
@@ -114,7 +436,7 @@ def decide_trade(current_price, upper_bollinger_band, lower_bollinger_band):
     global witness_input_complete
 
     print('Deciding on the trade')
-    if (current_price > (upper_bollinger_band / 100) * (100 - config.UPPER_BOUND_PERCENTAGE)):
+    if (current_price > (upper_bollinger_band / 100) * (100 - UPPER_BOUND_PERCENTAGE)):
         print("Selling token1")
         proof_args['boundPercentage'] = upperBoundPercentage
         proof_args['buySellFlag'] = 0
@@ -127,7 +449,7 @@ def decide_trade(current_price, upper_bollinger_band, lower_bollinger_band):
         print(witness_input)
         witness_input_complete = True
 
-    elif (current_price <= (lower_bollinger_band / 100) * (100 - config.LOWER_BOUND_PERCENTAGE)):
+    elif (current_price <= (lower_bollinger_band / 100) * (100 - LOWER_BOUND_PERCENTAGE)):
         print("Buying token1")
         proof_args['boundPercentage'] = lowerBoundPercentage
         proof_args['buySellFlag'] = 1
@@ -177,20 +499,20 @@ def sign_and_send_tx(tx_name, tx_args):
     event_name = ''
     if tx_name == 'test':
         tx = BotContract.functions.test().buildTransaction(
-            {'from': config.ACCOUNT, 'nonce': web3.eth.get_transaction_count(config.ACCOUNT)})
+            {'from': ACCOUNT, 'nonce': web3.eth.get_transaction_count(ACCOUNT)})
         event_name = 'TestEvent'
     elif tx_name == 'calculateIndicators':
         tx = BotContract.functions.calculateIndicators(tx_args['num_of_periods'], tx_args['period_length']).buildTransaction(
-            {'from': config.ACCOUNT, 'nonce': web3.eth.get_transaction_count(config.ACCOUNT)})
+            {'from': ACCOUNT, 'nonce': web3.eth.get_transaction_count(ACCOUNT)})
         event_name = 'BollingerIndicators'
     elif tx_name == 'trade':
         print('Sending the proof to the smart contract to get it verified')
         tx = BotContract.functions.trade(tx_args['a'], tx_args['b'], tx_args['c'], tx_args['inputs']).buildTransaction(
-            {'from': config.ACCOUNT, 'nonce': web3.eth.get_transaction_count(config.ACCOUNT)})
+            {'from': ACCOUNT, 'nonce': web3.eth.get_transaction_count(ACCOUNT)})
         event_name = 'ProofVerified'
 
     signed_txn = web3.eth.account.sign_transaction(
-        tx, private_key=config.ACCOUNT_KEY)
+        tx, private_key=ACCOUNT_KEY)
     tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
     worker = Thread(target=event_log_loop, args=(
         web3.toHex(tx_hash), event_name, 5))
@@ -201,10 +523,10 @@ def sign_and_send_tx(tx_name, tx_args):
 
 def trigger_trade(num_of_periods, period_length):
     price = BotContract.functions.getCurrentPrice().call(
-        {'from': config.ACCOUNT})
+        {'from': ACCOUNT})
     print('Current price is', price)
 
-    initial_balance = web3.eth.getBalance(config.ACCOUNT)
+    initial_balance = web3.eth.getBalance(ACCOUNT)
     print('Initial balance is', web3.fromWei(
         initial_balance, "ether"), 'ether')
 
@@ -269,32 +591,24 @@ def server_handler(args):
     server = VsockListener()
     server.bind(args.port)
     print("Started listening to port : ", str(args.port))
-    web3 = Web3(Web3.HTTPProvider(config.URL))
+    web3 = Web3(Web3.HTTPProvider(URL))
     web3.eth.set_gas_price_strategy(fast_gas_price_strategy)
-    contract_address = web3.toChecksumAddress(config.BOT_CONTRACT_ADDRESS)
+    contract_address = web3.toChecksumAddress(BOT_CONTRACT_ADDRESS)
     BotContract = web3.eth.contract(
-        abi=config.BOT_ABI, address=contract_address)
+        abi=BOT_ABI, address=contract_address)
     server.recv_data()
 
 
 def main():
     global web3
     global BotContract
-    web3 = Web3(Web3.HTTPProvider(config.URL))
+    web3 = Web3(Web3.HTTPProvider(URL))
     web3.eth.set_gas_price_strategy(fast_gas_price_strategy)
     print(web3)
-    contract_address = web3.toChecksumAddress(config.BOT_CONTRACT_ADDRESS)
+    contract_address = web3.toChecksumAddress(BOT_CONTRACT_ADDRESS)
     BotContract = web3.eth.contract(
-        abi=config.BOT_ABI, address=contract_address)
+        abi=BOT_ABI, address=contract_address)
     print(BotContract)
-    # trigger_trade(10,10)
-
-    # raw_msg = 'hey'
-    # sig = sign(raw_msg)
-    # msg = hashlib.sha512(raw_msg.encode("utf-8")).digest()
-    # pk = get_public_key()
-    # is_verified = pk.verify(sig, msg)
-    # print(is_verified)
 
     parser = argparse.ArgumentParser(prog='vsock-sample')
     parser.add_argument("--version", action="version",
