85936a599a014a8f0de99338a90b390a848b9be9
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2022-07-21 18:02:39 +0100

    Attempt to reduce docker image size

--- off-chain/host/run.sh
+++ off-chain/host/run.sh
@@ -2,4 +2,4 @@
 
 # To call the client, you have to pass: CID of the enclave, Port for remote server,
 # and a dummy string that will trigger the Nitro Enclave. For Example:
-python3 client.py client 16 5005 ""
+python3 client.py client 16 5005
--- off-chain/tee/README.md
+++
@@ -1,2 +0,0 @@
-# AWS Nitro Trusted Execution Encvironment
-
