0b835c785307e8fa38283740e3a3e6c450e56d6a
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2022-07-21 18:20:18 +0100

    Modified run script for the host

--- off-chain/host/run.sh
+++ off-chain/host/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Download Zokrates and set the path.
+curl -LSfs [get.zokrat.es](http://get.zokrat.es/) | sh
+export PATH=$PATH:/home/ec2-user/.zokrates/bin
+
 # To call the client, you have to pass: CID of the enclave, Port for remote server,
 # and a dummy string that will trigger the Nitro Enclave. For Example:
+
 python3 client.py client 16 5005
